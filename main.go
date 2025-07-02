7135377d9d2ced5129603061352ab44ac9059da1
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-07-02 23:41:05 +0530

    add main.go with basic HTTP server implementation

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+    fmt.Println("Starting server at port 8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        fmt.Println("Server failed:", err)
+    }
+}
