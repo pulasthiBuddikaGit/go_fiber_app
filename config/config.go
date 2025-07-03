e35036d2534f2860d0ef47bbe16e82b915373515
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-07-03 12:10:22 +0530

    refactor: update LoadConfig to use environment variables and add InitMongo function for MongoDB connection

--- config/config.go
+++ config/config.go
@@ -1,13 +1,26 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	MongoURI string
 	Database string
 }
 
 func LoadConfig() *Config {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("❌ Error loading .env file:", err)
+	}
+
 	return &Config{
-		MongoURI: "mongodb://localhost:27017", // or 127.0.0.1:27017
-		Database: "userDB",
+		MongoURI: os.Getenv("MongoDB_URL"),
+		Database: os.Getenv("MongoDB_DB"),
 	}
 }
---
+++ storage/mongo.go
@@ -0,0 +1,40 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/pulasthiBuddikaGit/go_fiber_app/config" // replace with your actual module name
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+var UserDB *mongo.Database
+
+func InitMongo(cfg *config.Config) {
+	// Create a context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Connect to MongoDB
+	clientOptions := options.Client().ApplyURI(cfg.MongoURI)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("MongoDB connection error:", err)
+	}
+
+	// Check the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("MongoDB ping failed:", err)
+	}
+
+	fmt.Println("✅ Connected to MongoDB")
+
+	Client = client
+	UserDB = client.Database(cfg.Database)
+}
