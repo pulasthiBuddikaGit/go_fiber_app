7fffadae3e777488a0ab607a0d4f9f33a98cadb4
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-07-03 12:10:32 +0530

    refactor: load configuration and initialize MongoDB connection in main function

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/pulasthiBuddikaGit/go_fiber_app/config"
+	"github.com/pulasthiBuddikaGit/go_fiber_app/storage"
 )
 
 func main() {
-    fmt.Println("Starting server at port 8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println("Server failed:", err)
-    }
+	cfg := config.LoadConfig()
+	storage.InitMongo(cfg)
+
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Server failed:", err)
+	}
 }
