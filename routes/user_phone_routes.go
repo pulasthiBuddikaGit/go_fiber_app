19381e3b730d21743466e4a248222e5b702052c3
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-07-07 23:15:30 +0530

    add: register route to update user phone numbers by ID

--- routes/user_phone_routes.go
+++ routes/user_phone_routes.go
@@ -12,6 +12,7 @@ func RegisterUserPhoneRoutes(app fiber.Router) {
 
 	userPhoneGroup.Post("/", handler.CreateUserPhoneHandler)
 	userPhoneGroup.Get("/:userId", handler.GetUserPhoneNumbersByUserIDHandler)
+	userPhoneGroup.Put("/:id", handler.UpdateUserPhoneHandler)
 	// userGroup.Get("/", handler.GetAllUsersHandler)
 	// userGroup.Get("/:id", handler.GetUserByIDHandler)
 	// userGroup.Put("/:id", handler.UpdateUserHandler)
