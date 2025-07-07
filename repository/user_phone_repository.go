3a01b71ab14a2b9866557b6231ed9d6667037bf7
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-07-07 22:12:37 +0530

    add: implement GetUserPhoneNumbersByUserID function to retrieve phone numbers by user ID

--- repository/user_phone_repository.go
+++ repository/user_phone_repository.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/pulasthiBuddikaGit/go_fiber_app/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -21,4 +23,33 @@ func CreateUserPhone(phone *model.UserPhone) (*mongo.InsertOneResult, error) {
 	defer cancel()
 
 	return userPhoneCollection.InsertOne(ctx, phone)
+}
+
+// GetUserPhoneNumbersByUserID fetches all phone numbers associated with a specific user ID
+func GetUserPhoneNumbersByUserID(userID string) ([]model.UserPhone, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	objID, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	filter := bson.M{"userId": objID}
+	cursor, err := userPhoneCollection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var phones []model.UserPhone
+	for cursor.Next(ctx) {
+		var phone model.UserPhone
+		if err := cursor.Decode(&phone); err != nil {
+			return nil, err
+		}
+		phones = append(phones, phone)
+	}
+
+	return phones, nil
 }
