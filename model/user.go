ed13671b98982cf415aac5518cd40491f08cb90f
pulasthiBuddikaGit <balasooriyagepulasthi@gmail.com>
2025-07-08 10:40:29 +0530

    add: restructure User model to include password, createdAt, updatedAt, and deletedAt fields

--- model/user.go
+++ model/user.go
@@ -1,10 +1,19 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 
 // User represents a user document in the "users" collection
 type User struct {
 	ID    primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name  string             `bson:"name" json:"name"`
 	Email string             `bson:"email" json:"email"`
+	Password  string              `bson:"password,omitempty" json:"password,omitempty"` // Omit from JSON responses by default
+	CreatedAt time.Time           `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time           `bson:"updatedAt" json:"updatedAt"`
+	DeletedAt *time.Time          `bson:"deletedAt,omitempty" json:"deletedAt,omitempty"` // Nil if not deleted (soft delete)
 }
